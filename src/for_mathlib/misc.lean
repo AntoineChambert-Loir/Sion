9ffedb7e4db8471916849af7bc28ac8f30f5bc70
Antoine Chambert-Loir <antoine.chambert-loir@math.univ-paris-diderot.fr>
2023-05-14 12:49:20 +0200

    une simplification qui ne simplifie pas

--- src/for_mathlib/misc.lean
+++ src/for_mathlib/misc.lean
@@ -86,44 +86,46 @@ begin
     exact ⟨is_open_induced hu_open, hat⟩, },
 end
 
--- Essayons de faire plus simple
+-- Essayons de faire plus simple (pas convaincant!)
 example {α : Type*} [topological_space α] (s t : set α) (hst : s ⊆ t) (J : set s) (a : ↥s) : cluster_pt a (filter.principal J) ↔ ∃ᶠ x in nhds_within a t, ∃ (h : x ∈ s), (⟨x, h⟩ : s) ∈ J  := 
 begin
   simp only [cluster_pt_principal_iff_frequently, filter.frequently, not_iff_not, filter.eventually, mem_nhds_iff, mem_nhds_within],
   simp only [exists_prop, not_exists],
-  suffices : ∀ (t : set s), 
-    t ⊆ { x : s | x ∉ J} ∧ is_open t ∧ a ∈ t 
-    ↔ (λ u, (u ⊆ coe '' {x : s | x ∉ J} ∧ is_open u ∧ ↑a ∈ u) : set α → Prop) (coe '' t),
-  rw exists_congr this, 
-  rw subtype.exists_set_subtype (λ u, (u ⊆ coe '' {x : s | x ∉ J} ∧ is_open u ∧ ↑a ∈ u) : set α → Prop),
-  apply exists_congr, 
-  intro u,
   
+  simp_rw (inducing_coe).is_open_iff, 
+  simp_rw and.comm, simp_rw ← and.assoc, 
+  simp_rw ← exists_and_distrib_left ,  
+  rw exists_comm, 
+  apply exists_congr, intro u,
+  simp_rw and.comm, simp_rw and.assoc, simp_rw exists_and_distrib_left,
+  rw and.congr_right_iff,
+  intro hu_open,
   split,
-  { rintro ⟨v, hv_subset, hv_open, hav⟩,
-    obtain ⟨u, hu, hut⟩ := (inducing_coe).is_open_iff.mp hv_open, 
-    use u, 
-    apply and.intro hu,
-    simp only [←hut, set.mem_preimage] at hav, 
-    apply and.intro hav, 
-    intros x hx, 
-    simp only [set.mem_set_of_eq], 
-    intro hxs, 
-    apply hv_subset, 
-    rw ← hut,  
-    rw set.mem_preimage,
-    rw set.mem_inter_iff at hx, exact hx.1, },
-  { rintro ⟨u, hu_open, hat, hut_subset⟩,
-    use coe ⁻¹' u,
-    split, 
-    rintros ⟨x, hx⟩ hx', rw set.mem_preimage at hx', 
-    apply hut_subset, 
-    exact ⟨hx', hst hx⟩, 
-    exact ⟨is_open_induced hu_open, hat⟩, },
+  { rintro ⟨x, hux, hax, hx⟩,
+    simp only [←hux, set.mem_preimage] at hax,  
+    apply and.intro hax, 
+    intros y hyut hys, 
+    apply hx, simp only [← hux, set.mem_preimage], exact hyut.1, },
+  { rintros ⟨hau, hut⟩, 
+    use coe ⁻¹' u, 
+    apply and.intro rfl,
+    rw set.mem_preimage, apply and.intro hau,
+    intro y, rw set.mem_preimage, intro hy, 
+    simp, rw ← subtype.coe_eta y y.prop,
+    apply hut, 
+    exact ⟨hy, hst y.prop⟩, },
 end
 
 
 end filter
+
+example {α : Type*} (p : α→ Prop ) (a : α) :
+  ∃ (x : α), p x ∧ x = a ↔  p a :=
+begin
+
+library_search
+end
+
 
 -- Not needed actually...
 
